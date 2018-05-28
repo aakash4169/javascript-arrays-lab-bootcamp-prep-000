f1d5901b4ab08c222c81f9f2d7bf2ab123acbc32
akki4169@gmail.com Kulkarni <akki4169@gmail.com>
2018-05-28 05:57:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 const app = "I don't do much."
 //var kittens=["Milo","Otis","Garfield"];
 function destructivelyAppendKitten(name){
-  kittens.push(name);
+   kittens=[...kittens,name];
   return kittens;
 }
+function destructivelyPrependKitten(name){
+  kittens=[name,...kittens];
+  return kittens;
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return 
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return 
+}
+function appendKitten(name){
+  var kit=[...kittens,name];
+  return kit;
+}
+function prependKitten(name){
+  var kit=[name,...kittens];
+  return kit;
+}
+
+function removeLastKitten(){
+  var kit=kittens.slice(0,kittens.length-1);
+  return kit;
+}
+function removeFirstKitten(){
+  var kit=kittens.slice(1,kittens.length);
+  return kit;
+}
+removeLastKitten();
